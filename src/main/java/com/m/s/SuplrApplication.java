b58c2c1d23c0aade38aaebe34a4a3ace8382f3c2
Mayesamomo <48134632+Mayesamomo@users.noreply.github.com>
2020-04-11 14:35:38 +0100

    added user class

---
+++ src/main/java/com/m/s/SuplrApplication.java
@@ -0,0 +1,13 @@
+package com.m.s;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuplrApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SuplrApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/m/s/SuplrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.m.s;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuplrApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
